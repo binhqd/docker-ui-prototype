1f5a5afa633148a04306c2ba439029043dd18c7c
binhqd <binhqd@gmail.com>
2016-06-30 10:28:21 +0700

    Add commands: - workflow add item - workflow link id1 id2

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,7 @@ var di = {
 
 
 var searchTimeout;
+
 function doSearch(event) {
   if (searchTimeout)
     clearTimeout(searchTimeout);
@@ -21,7 +22,7 @@ function doSearch(event) {
       return;
     }
 
-    var url = 'https://hub.docker.com/v2/search/repositories/?page=1&query='+encodeURIComponent(searchTerm)+'&page_size=20';
+    var url = 'https://hub.docker.com/v2/search/repositories/?page=1&query=' + encodeURIComponent(searchTerm) + '&page_size=20';
 
     $('#listDockerImages ul').remove();
     $('#listDockerImages').append($('<ul class="list-result"><li>Looking for docker images ...</li></ul>'));
@@ -42,7 +43,7 @@ function doSearch(event) {
 $(document).ready(function() {
   $('#txtSearchDockerImages').keyup(doSearch);
 
-  var source   = $("#listDockerImageItemTemplate").html();
+  var source = $("#listDockerImageItemTemplate").html();
   template = Handlebars.compile(source);
 
 
@@ -54,7 +55,7 @@ $(document).ready(function() {
     $('.tab-content').removeClass('current');
 
     $(this).addClass('current');
-    $("#"+tab_id).addClass('current');
+    $("#" + tab_id).addClass('current');
   });
 
   // Resizable
@@ -66,36 +67,257 @@ $(document).ready(function() {
 
 
   // testing only
-  // var response = {"count": 3756, "next": "https://hub.docker.com/v2/search/repositories/?query=mysql&page=2&page_size=20", "previous": null, "results": [{"star_count": 2547, "pull_count": 20803174, "repo_owner": null, "short_description": "MySQL is a widely used, open-source relational database management system (RDBMS).", "is_automated": false, "is_official": true, "repo_name": "mysql"}, {"star_count": 164, "pull_count": 211591, "repo_owner": null, "short_description": "Optimized MySQL Server Docker images. Created, maintained and supported by the MySQL team at Oracle", "is_automated": true, "is_official": false, "repo_name": "mysql/mysql-server"}, {"star_count": 0, "pull_count": 1191, "repo_owner": null, "short_description": "MySQL (MariaDB fork) Docker image.", "is_automated": true, "is_official": false, "repo_name": "tozd/mysql"}, {"star_count": 45, "pull_count": 5857293, "repo_owner": null, "short_description": "Image containing mysql. Optimized to be linked to another image/container.", "is_automated": true, "is_official": false, "repo_name": "centurylink/mysql"}, {"star_count": 8, "pull_count": 48941, "repo_owner": null, "short_description": "Centos/Debian Based Customizable MySQL Container - Updated 06/16/2016", "is_automated": true, "is_official": false, "repo_name": "appcontainers/mysql"}, {"star_count": 2, "pull_count": 729, "repo_owner": null, "short_description": "Docker Mysql", "is_automated": true, "is_official": false, "repo_name": "alterway/mysql"}, {"star_count": 2, "pull_count": 499, "repo_owner": null, "short_description": "MySQL for Drupal", "is_automated": true, "is_official": false, "repo_name": "drupaldocker/mysql"}, {"star_count": 2, "pull_count": 395, "repo_owner": null, "short_description": "Yfix docker built mysql", "is_automated": true, "is_official": false, "repo_name": "yfix/mysql"}, {"star_count": 1, "pull_count": 486, "repo_owner": null, "short_description": "MySQL server image", "is_automated": true, "is_official": false, "repo_name": "phpmentors/mysql"}, {"star_count": 0, "pull_count": 247670, "repo_owner": null, "short_description": "Improved `mysql` service with support for `mysqld_safe` and `fixtures` loaded from `mysqldump.sql`", "is_automated": true, "is_official": false, "repo_name": "cloudposse/mysql"}, {"star_count": 0, "pull_count": 805, "repo_owner": null, "short_description": "MySQL service for nanobox.io", "is_automated": true, "is_official": false, "repo_name": "nanobox/mysql"}, {"star_count": 36, "pull_count": 1192550, "repo_owner": null, "short_description": "", "is_automated": true, "is_official": false, "repo_name": "sameersbn/mysql"}, {"star_count": 0, "pull_count": 2111, "repo_owner": null, "short_description": "Build for MySQL. Project available on https://github.com/vukor/docker-web-stack", "is_automated": true, "is_official": false, "repo_name": "vukor/mysql"}, {"star_count": 6, "pull_count": 1485, "repo_owner": null, "short_description": "MySQL Server based on Ubuntu 14.04", "is_automated": true, "is_official": false, "repo_name": "marvambass/mysql"}, {"star_count": 0, "pull_count": 1770, "repo_owner": null, "short_description": "MySQL is a widely used, open-source relational database management system (RDBMS).", "is_automated": true, "is_official": false, "repo_name": "lancehudson/docker-mysql"}, {"star_count": 1, "pull_count": 104, "repo_owner": null, "short_description": "MySQL images with my own config files.", "is_automated": true, "is_official": false, "repo_name": "sin30/mysql"}, {"star_count": 2, "pull_count": 3131, "repo_owner": null, "short_description": "Docker image to run MySQL by Azuki - http://azk.io", "is_automated": true, "is_official": false, "repo_name": "azukiapp/mysql"}, {"star_count": 1, "pull_count": 75, "repo_owner": null, "short_description": "mysql", "is_automated": true, "is_official": false, "repo_name": "kaluzki/mysql"}, {"star_count": 0, "pull_count": 113, "repo_owner": null, "short_description": "akilli/base based MySQL image", "is_automated": true, "is_official": false, "repo_name": "akilli/mysql"}, {"star_count": 0, "pull_count": 208, "repo_owner": null, "short_description": "MySQL", "is_automated": true, "is_official": false, "repo_name": "livingobjects/mysql"}]};
-  // var html = template(response);
-  //
-  // $('#listDockerImages ul').remove();
-  // $('#listDockerImages').append(html);
-  //
-  // applyDragDrop();
+  var response = {
+    "count": 3756,
+    "next": "https://hub.docker.com/v2/search/repositories/?query=mysql&page=2&page_size=20",
+    "previous": null,
+    "results": [{
+      "star_count": 2547,
+      "pull_count": 20803174,
+      "repo_owner": null,
+      "short_description": "MySQL is a widely used, open-source relational database management system (RDBMS).",
+      "is_automated": false,
+      "is_official": true,
+      "repo_name": "mysql"
+    }, {
+      "star_count": 164,
+      "pull_count": 211591,
+      "repo_owner": null,
+      "short_description": "Optimized MySQL Server Docker images. Created, maintained and supported by the MySQL team at Oracle",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "mysql/mysql-server"
+    }, {
+      "star_count": 0,
+      "pull_count": 1191,
+      "repo_owner": null,
+      "short_description": "MySQL (MariaDB fork) Docker image.",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "tozd/mysql"
+    }, {
+      "star_count": 45,
+      "pull_count": 5857293,
+      "repo_owner": null,
+      "short_description": "Image containing mysql. Optimized to be linked to another image/container.",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "centurylink/mysql"
+    }, {
+      "star_count": 8,
+      "pull_count": 48941,
+      "repo_owner": null,
+      "short_description": "Centos/Debian Based Customizable MySQL Container - Updated 06/16/2016",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "appcontainers/mysql"
+    }, {
+      "star_count": 2,
+      "pull_count": 729,
+      "repo_owner": null,
+      "short_description": "Docker Mysql",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "alterway/mysql"
+    }, {
+      "star_count": 2,
+      "pull_count": 499,
+      "repo_owner": null,
+      "short_description": "MySQL for Drupal",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "drupaldocker/mysql"
+    }, {
+      "star_count": 2,
+      "pull_count": 395,
+      "repo_owner": null,
+      "short_description": "Yfix docker built mysql",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "yfix/mysql"
+    }, {
+      "star_count": 1,
+      "pull_count": 486,
+      "repo_owner": null,
+      "short_description": "MySQL server image",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "phpmentors/mysql"
+    }, {
+      "star_count": 0,
+      "pull_count": 247670,
+      "repo_owner": null,
+      "short_description": "Improved `mysql` service with support for `mysqld_safe` and `fixtures` loaded from `mysqldump.sql`",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "cloudposse/mysql"
+    }, {
+      "star_count": 0,
+      "pull_count": 805,
+      "repo_owner": null,
+      "short_description": "MySQL service for nanobox.io",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "nanobox/mysql"
+    }, {
+      "star_count": 36,
+      "pull_count": 1192550,
+      "repo_owner": null,
+      "short_description": "",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "sameersbn/mysql"
+    }, {
+      "star_count": 0,
+      "pull_count": 2111,
+      "repo_owner": null,
+      "short_description": "Build for MySQL. Project available on https://github.com/vukor/docker-web-stack",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "vukor/mysql"
+    }, {
+      "star_count": 6,
+      "pull_count": 1485,
+      "repo_owner": null,
+      "short_description": "MySQL Server based on Ubuntu 14.04",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "marvambass/mysql"
+    }, {
+      "star_count": 0,
+      "pull_count": 1770,
+      "repo_owner": null,
+      "short_description": "MySQL is a widely used, open-source relational database management system (RDBMS).",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "lancehudson/docker-mysql"
+    }, {
+      "star_count": 1,
+      "pull_count": 104,
+      "repo_owner": null,
+      "short_description": "MySQL images with my own config files.",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "sin30/mysql"
+    }, {
+      "star_count": 2,
+      "pull_count": 3131,
+      "repo_owner": null,
+      "short_description": "Docker image to run MySQL by Azuki - http://azk.io",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "azukiapp/mysql"
+    }, {
+      "star_count": 1,
+      "pull_count": 75,
+      "repo_owner": null,
+      "short_description": "mysql",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "kaluzki/mysql"
+    }, {
+      "star_count": 0,
+      "pull_count": 113,
+      "repo_owner": null,
+      "short_description": "akilli/base based MySQL image",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "akilli/mysql"
+    }, {
+      "star_count": 0,
+      "pull_count": 208,
+      "repo_owner": null,
+      "short_description": "MySQL",
+      "is_automated": true,
+      "is_official": false,
+      "repo_name": "livingobjects/mysql"
+    }]
+  };
+  var html = template(response);
+
+  $('#listDockerImages ul').remove();
+  $('#listDockerImages').append(html);
+
+  applyDragDrop();
+
+  $('#hostCommandInput').keyup(function(event) {
+    if (event.which == 13) {
+      var val = $('#hostCommandInput').val();
+      if (val.trim() != '') {
+        sendHostCommand(val);
+      }
+    }
+  })
 });
 
+var cmdHistory = [];
+
+function isContainerExisted(idOrName) {
+  for (var key in nodes._data) {
+    if (key == idOrName) {
+      return true;
+    }
+  }
+  return false;
+}
+
+var CommandLogger = {
+  invalidCommand: function(cmd) {
+    $('#containerHostCommandInput').before($('<div class="cmd"><i>' + cmd + '</i>: Invalid command</div>'));
+  },
+  containerNotExist: function(idOrName) {
+    $('#containerHostCommandInput').before($('<div class="cmd"><i>Container \"' + idOrName + '\" not exist</i></div>'));
+  },
+  log: function(str) {
+    $('#containerHostCommandInput').before($('<div class="cmd">' + str + '</div>'));
+  }
+}
+
+
+function sendHostCommand(cmd) {
+  var addContainerPattern = /^workflow add ([\w\-\/\:\d]*)$/;
+  var linkContainerPattern = /^workflow link ([\w\-\/\:\d]*) ([\w\-\/\:\d]*)$/;
+  var testAdd = cmd.match(addContainerPattern);
+
+  if (testAdd) {
+    var imageName = testAdd[1];
+    addNode(imageName, {});
+    $('#containerHostCommandInput').before($('<div class="cmd">' + cmd + '</div>'));
+  } else if (cmd.match(linkContainerPattern)) {
+    var testLink = cmd.match(linkContainerPattern);
+
+    linkContainers(testLink[1], testLink[2]);
+  } else {
+    CommandLogger.invalidCommand(cmd);
+  }
+
+  cmdHistory.push(cmd);
+  // clean up
+  $('#hostCommandInput').val('');
+}
+
 function applyDragDrop() {
-  $( "#listDockerImages li" ).draggable({
+  $("#listDockerImages li").draggable({
     helper: "clone"
   });
 
-  $( "#myCanvas" ).droppable({
+  $("#myCanvas").droppable({
     tolerance: 'pointer',
     drop: function(event, ui) {
       console.log(event, ui);
 
-      var canvasOffset = $( "#myCanvas canvas" ).offset();
-      addNode(nodes.length, ui.helper.attr('data-tag'), {
+      var canvasOffset = $("#myCanvas canvas").offset();
+      addNode(ui.helper.attr('data-tag'), {
         description: '',
-        x: ui.offset.left - canvasOffset.left - ($( "#myCanvas canvas" ).width() / 2) + 150,
-        y: ui.offset.top - canvasOffset.top - ($( "#myCanvas canvas" ).height() / 2) + 50
+        x: ui.offset.left - canvasOffset.left - ($("#myCanvas canvas").width() / 2) + 150,
+        y: ui.offset.top - canvasOffset.top - ($("#myCanvas canvas").height() / 2) + 50
       });
     }
   });
 }
 
-var nodesData = [], edgesData = [];
+var nodesData = [],
+  edgesData = [];
 var seed = 2;
 
 function destroy() {
@@ -105,23 +327,61 @@ function destroy() {
   }
 }
 
-function addNode(id, label, options) {
+function getNextId() {
+  var id = 0;
+  for (var key in nodes._data) {
+    if (id === 0) {
+      id = parseInt(nodes._data[key].id);
+    } else {
+      var _currentID = parseInt(nodes._data[key].id);
+      if (_currentID > id) {
+        id = _currentID;
+      }
+    }
+  }
+
+  return ++id;
+}
+
+function linkContainers(srcID, desId) {
+  // check if id1 && id2 has existed
+  if (!isContainerExisted(srcID)) {
+    CommandLogger.containerNotExist(srcID);
+  } else if (!isContainerExisted(desId)) {
+    CommandLogger.containerNotExist(desId);
+  } else {
+    edges.add({
+      from: srcID,
+      to: desId
+    });
+  }
+}
+
+function addNode(label, options) {
+  var id = getNextId();
   try {
     var container = {
       id: id,
-      label: label,
+      label: label + '\nID: ' + id,
+      font: {
+        align: 'left'
+      },
       tag: label,
       description: options.description,
-      shape: 'box',
-      x: options.x,
-      y: options.y
+      shape: 'box'
     };
+
+    if (!isNaN(options.x)) {
+      container.x = options.x;
+    }
+    if (!isNaN(options.y)) {
+      container.y = options.y;
+    }
 
     nodes.add(container);
 
     hashContainers.push(new Container(container));
-  }
-  catch (err) {
+  } catch (err) {
     alert(err);
   }
 }
@@ -140,20 +400,25 @@ function draw() {
   nodes.add(nodesData);
   edges.add(edgesData);
   var data = {
-      nodes: nodes,
-      edges: edges
+    nodes: nodes,
+    edges: edges
   };
 
   // create a network
   var container = document.getElementById('myCanvas');
   var options = {
-    layout: {randomSeed:seed}, // just to make sure the layout is the same when the locale is changed
+    layout: {
+      randomSeed: seed
+    }, // just to make sure the layout is the same when the locale is changed
     locale: 'en',
     height: '100%',
     width: '100%',
     edges: {
       arrows: {
-        to: {enabled: true, scaleFactor: 1}
+        to: {
+          enabled: true,
+          scaleFactor: 1
+        }
       },
       shadow: true,
       smooth: {
@@ -172,19 +437,18 @@ function draw() {
           maxVisible: 30,
           drawThreshold: 5
         },
-        customScalingFunction: function (min,max,total,value) {
+        customScalingFunction: function(min, max, total, value) {
           if (max === min) {
             return 0.5;
-          }
-          else {
+          } else {
             var scale = 1 / (max - min);
-            return Math.max(0,(value - min)*scale);
+            return Math.max(0, (value - min) * scale);
           }
         }
       },
       selectionWidth: 4
     },
-    interaction:{
+    interaction: {
       dragView: false,
       zoomView: false,
       selectConnectedEdges: false
@@ -192,7 +456,7 @@ function draw() {
     manipulation: {
       enabled: true,
       initiallyActive: true,
-      addNode: function (data, callback) {
+      addNode: function(data, callback) {
         // filling in the popup DOM elements
         document.getElementById('operation').innerHTML = "Add Node";
         document.getElementById('node-id').value = data.id;
@@ -201,28 +465,27 @@ function draw() {
         document.getElementById('cancelButton').onclick = clearPopUp.bind();
         document.getElementById('network-popUp').style.display = 'block';
       },
-      editNode: function (data, callback) {
+      editNode: function(data, callback) {
         // filling in the popup DOM elements
         document.getElementById('operation').innerHTML = "Edit Node";
         document.getElementById('node-id').value = data.id;
         document.getElementById('node-label').value = data.label;
         document.getElementById('saveButton').onclick = saveData.bind(this, data, callback);
-        document.getElementById('cancelButton').onclick = cancelEdit.bind(this,callback);
+        document.getElementById('cancelButton').onclick = cancelEdit.bind(this, callback);
         document.getElementById('network-popUp').style.display = 'block';
       },
-      addEdge: function (data, callback) {
+      addEdge: function(data, callback) {
         if (data.from == data.to) {
           var r = confirm("Do you want to connect the node to itself?");
           if (r == true) {
             callback(data);
           }
-        }
-        else {
+        } else {
           callback(data);
         }
       }
     },
-    physics:{
+    physics: {
       solver: 'barnesHut',
       enabled: false
     }
@@ -231,17 +494,17 @@ function draw() {
   rivets.binders.input = {
     publishes: true,
     routine: rivets.binders.value.routine,
-    bind: function (el) {
-        el.addEventListener('input', this.publish);
+    bind: function(el) {
+      el.addEventListener('input', this.publish);
     },
-    unbind: function (el) {
-        el.removeEventListener('input', this.publish);
+    unbind: function(el) {
+      el.removeEventListener('input', this.publish);
     }
   };
 
   network = new vis.Network(container, data, options);
 
-  network.on('release', function(a,b,c,d) {
+  network.on('release', function(a, b, c, d) {
     console.log(a, b, c, d);
   });
   network.on("select", function(params) {
@@ -255,7 +518,9 @@ function draw() {
         di.selected = null;
       }
 
-      rivets.bind($('#containerProps'), {prop: di.selected})
+      rivets.bind($('#containerProps'), {
+        prop: di.selected
+      })
     } else { // if selected item is an edge
 
     }
@@ -273,7 +538,7 @@ function cancelEdit(callback) {
   callback(null);
 }
 
-function saveData(data,callback) {
+function saveData(data, callback) {
   data.id = document.getElementById('node-id').value;
   data.label = document.getElementById('node-label').value;
   clearPopUp();
@@ -281,7 +546,7 @@ function saveData(data,callback) {
 }
 
 Handlebars.registerHelper('isEmpty', function(arr, options) {
-  if(arr.length == 0) {
+  if (arr.length == 0) {
     return options.fn(this);
   }
   return options.inverse(this);
